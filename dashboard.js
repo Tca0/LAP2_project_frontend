81c9baf797b06c71f7e71d366db25497d59c475d
aimyv <av533@bath.ac.uk>
2023-01-06 22:27:04 +0000

    testing padding

--- dashboard.js
+++ dashboard.js
@@ -160,7 +160,8 @@ async function updateExp(id) {
         document.getElementsByClassName('streak').textContent = `${data.frequency.toUpperCase()} ${data.streak} 🔥`;
         const plusButton = document.getElementById(id);
         plusButton.replaceWith(plusButton.cloneNode(true));
-        location.reload()
+        document.getElementById("habitDetails").style.padding = 0;
+        location.reload();
       }
       if (data.err){ throw Error(data.err); }
   } catch (err) {
